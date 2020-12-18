00cfa4ac3f0b4c25a46408e827c6a1afe6c6cc89
20161020503 <santirian@hotmail.com>
2020-12-18 15:09:55 -0500

    Update main.ts

--- angular-app/src/main.ts
+++ angular-app/src/main.ts
@@ -4,6 +4,20 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+
+
+fetch('https://httpbin.org/ip')
+    .then(function(response) {
+        return response.text();
+    })
+    .then(function(data) {
+        console.log('data = ', data);
+    })
+    .catch(function(err) {
+        console.error(err);
+    });
+
+
 if (environment.production) {
   enableProdMode();
 }
